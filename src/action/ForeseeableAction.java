d2ce11b2ef521793453b1ab433e1756099d6a787
Lambin Abigail <lambin@a13p9.fil.univ-lille1.fr>
2015-10-09 11:09:00 +0200

    Scheduler class

---
+++ src/action/ForeseeableAction.java
@@ -0,0 +1,12 @@
+package action;
+
+public class ForeseeableAction {
+
+	protected int totalTime;
+	protected int remainingTime;
+	
+	public ForeseeableAction(int totalTime){
+		this.totalTime = totalTime;
+	}
+	
+}
