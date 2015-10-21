7fbda1340e03390855fe7b9a848ccdbfc080b285
loic <loic@debian>
2015-10-21 20:32:44 +0200

    “TakeResourceActionTest”

--- tests/action/TakeResourceActionTest.java
+++ tests/action/TakeResourceActionTest.java
@@ -1,12 +1,16 @@
 package action;
 
+import pool.BasketPool;
+import pool.resource.*;
+
+import pool.user.ResourcefulUser;
+
 public class TakeResourceActionTest extends ActionTest{
 
 	@Override
 	public Action createAction() {
-		return;
+		return new TakeResourceAction<Basket>(new ResourcefulUser<Basket>(),new BasketPool(0));
 	}
-	
 	
 
 }
