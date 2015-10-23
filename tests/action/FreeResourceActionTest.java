7cad8327305dba89905267fd9868dc2c227bf48b
ThibaultAr <thibault.arloing@hotmail.fr>
2015-10-23 15:26:30 +0200

    ResourceActionTest -> cf README.md TODO

--- tests/action/FreeResourceActionTest.java
+++ tests/action/FreeResourceActionTest.java
@@ -1,47 +1,48 @@
 package action;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-import exception.ActionFinishedException;
 
 import pool.BasketPool;
 import pool.ResourcePool;
-import pool.resource.*;
-
+import pool.resource.Basket;
 import pool.user.ResourcefulUser;
+import exception.ActionFinishedException;
 
-public class TakeResourceActionTest extends ResourceActionTest{
+public class FreeResourceActionTest extends ResourceActionTest{
+	public static Basket b = new Basket();
+	public class MockUser extends ResourcefulUser<Basket>{
+
+		public Basket getResource(){
+			return b ;
+		}
+	}
+	
 	public class MockPool extends BasketPool{
-		protected Basket b = new Basket();
 		public MockPool(){
 			super(1);
-		}
-		
-		public Basket provideResource(){
-			return b;
+			this.givenResources.add(b);
 		}
 	}
 	
 	@Override
 	public ResourceAction<Basket> createBasketAction(
 			ResourcefulUser<Basket> user, ResourcePool<Basket> pool) {
-		return new TakeResourceAction<Basket>(user,pool);
+		return new FreeResourceAction<Basket>(new MockUser(),new MockPool());
 	}
 
 
 	@Test
 	public void resourceActionTest() throws ActionFinishedException {
-		ResourcefulUser<Basket> user = new ResourcefulUser<Basket>();
+		ResourcefulUser<Basket> user = new MockUser();
 		ResourcePool<Basket> pool = new MockPool();
+		
 		
 		ResourceAction<Basket> action = createBasketAction(user, pool);
 		
-		assertNull(user.getResource());
 		
-		action.doStep();
+		assertNotNull(user.getResource());
 		
-		assertSame(user.getResource(), pool.provideResource());	
+		action.doStep();	
 	}
 }
